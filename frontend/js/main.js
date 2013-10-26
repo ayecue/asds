93eb2164d3eae37097f12bef4e4f1160137d8172
Snowflame <borkowskiberlin@gmail.com>
2013-10-26 21:43:30 +0200

    next Step of Layout

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,43 @@
 $(function(){
-	console.log("start System");
 
+	// Public Informations
+	var userName = "swagger"
+
+	// Station Section 
+	function loadStationInformations(id){
+		$(".greyLayer").fadeIn("fast", function(){
+				$.mobile.loading( 'show' );
+				$.ajax({
+					url: "station.json",
+					context: document.body
+				}).done(function(data) {
+					$(".ownerName").text(data.owner);
+					$("#stationDetail").attr("data-active-station",data.id);
+					$(".stationName").text(data.name);
+
+					if(data.owner == userName){
+						$('[data-role="button"][data-icon="plus"]').hide();
+					} else {
+						$('[data-role="button"][data-icon="minus"]').hide();
+					}
+
+					$.mobile.changePage('#stationDetail', 'pop', true, true);
+					$(".greyLayer").fadeOut( 'fast',function(){
+						$.mobile.loading( 'hide' );
+					});
+				});
+
+		});
+	}
+
+	function addOwnStation(id, name){
+		var liObjekt = '<li><a href="#" data-station-id="'+id+'">'+name+'</a></li>';
+		$("#stationContent").append(liObjekt);
+		$('[data-station-id="'+id+'"]').click(function(){loadStationInformations(id)});
+	}
+
+
+	// Money History
 	function addHistoryMoney(value, Comment, transferDate){
 		var classMoney = "";
 		if(value < 0){
@@ -13,17 +50,18 @@ $(function(){
 		$("#moneyHistoryContent").append(liObjekt);
 	}
 
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
-	addHistoryMoney(-45,"Swag", "gestern");
+	// Event Hitory
+	function addHistoryEvent(Comment, transferDate){
+		var liObjekt = '<li>'+Comment+' | '+transferDate+'</li>';
+
+		$("#eventHistoryContent").append(liObjekt);
+	}
+
+
+	addOwnStation("17", "Station of Death");
+	addOwnStation("18", "Station of Death");
+	addOwnStation("19", "Station of Death");
+	addOwnStation("20", "Station of Death");
+	addOwnStation("16", "Station of Death");
+	addOwnStation("15", "Station of Death");
 });
