aeb2418096e76660bc4dbb4956e957d4858046cc
Snowflame <borkowskiberlin@gmail.com>
2013-10-26 23:35:07 +0200

    update Layout

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -17,6 +17,7 @@ $(function(){
 
 					if(data.owner == userName){
 						$('[data-role="button"][data-icon="plus"]').hide();
+						$(".ownerName").text("you are the Owner");
 					} else {
 						$('[data-role="button"][data-icon="minus"]').hide();
 					}
@@ -30,12 +31,35 @@ $(function(){
 		});
 	}
 
-	function addOwnStation(id, name){
-		var liObjekt = '<li><a href="#" data-station-id="'+id+'">'+name+'</a></li>';
-		$("#stationContent").append(liObjekt);
-		$('[data-station-id="'+id+'"]').click(function(){loadStationInformations(id)});
+	function loadOwnStations(){
+		if($("#stations").attr("data-loaded") == "false"){
+			$(".greyLayer").fadeIn("fast", function(){
+					$.mobile.loading( 'show' );
+					$.ajax({
+						url: "ownStation.json",
+						context: document.body
+					}).done(function(data) {
+
+						$.each(data, function(index, value) {
+							addOwnStation(value[0].id, value[0].name);
+						}); 
+						$("#stationContent").listview('refresh');
+
+						$(".greyLayer").fadeOut( 'fast',function(){
+							$("#stations").attr("data-loaded","true")
+							$.mobile.loading( 'hide' );
+						});
+					});
+
+			});
+		}
 	}
 
+	function addOwnStation(id, name){
+		var liObjekt = '<li><a href="#" class="ui-link-inherit" data-station-id="'+id+'">'+name+'</a></li>';
+		$("#stationContent").prepend(liObjekt);
+		$('[data-station-id="'+id+'"]').click(function(){loadStationInformations(id)});
+	}
 
 	// Money History
 	function addHistoryMoney(value, Comment, transferDate){
@@ -54,14 +78,33 @@ $(function(){
 	function addHistoryEvent(Comment, transferDate){
 		var liObjekt = '<li>'+Comment+' | '+transferDate+'</li>';
 
-		$("#eventHistoryContent").append(liObjekt);
+		$("#eventHistoryContent").prepend(liObjekt);
 	}
 
+	function loadEvents(){
+		if($("#eventHistory").attr("data-loaded") == "false"){
+			$(".greyLayer").fadeIn("fast", function(){
+					$.mobile.loading( 'show' );
+					$.ajax({
+						url: "events.json",
+						context: document.body
+					}).done(function(data) {
 
-	addOwnStation("17", "Station of Death");
-	addOwnStation("18", "Station of Death");
-	addOwnStation("19", "Station of Death");
-	addOwnStation("20", "Station of Death");
-	addOwnStation("16", "Station of Death");
-	addOwnStation("15", "Station of Death");
+						$.each(data, function(index, value) {
+							addHistoryEvent(value[0].comment,value[0].transferDate);
+						}); 
+						$("#eventHistoryContent").listview('refresh');
+
+						$(".greyLayer").fadeOut( 'fast',function(){
+							$("#eventHistory").attr("data-loaded","true")
+							$.mobile.loading( 'hide' );
+						});
+					});
+
+			});
+		}
+	}
+
+ 	$('#stations').bind('pageshow', loadOwnStations );
+ 	$('#eventHistory').bind('pageshow', loadEvents );
 });
